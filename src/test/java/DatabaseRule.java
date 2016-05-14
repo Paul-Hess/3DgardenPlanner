e88d71ec233351748e11060a14cceec783e899d0
paul-hess <pauljacksonhess@gmail.com>
2016-05-13 22:20:52 -0700

    add basic file structure

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -5,14 +5,20 @@ public class DatabaseRule extends ExternalResource {
 
   @Override
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/db_name_test", null, null);
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/three_d_garden_test", null, null);
   }
 
   @Override
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String deleteTableQuery = "DELETE FROM table *;";
-      con.createQuery(deleteTableQuery).executeUpdate();
+      String deleteUserQuery = "DELETE FROM users *;";
+      String deleteGardenQuery = "DELETE FROM gardens *;";
+      String deletePlantQuery = "DELETE FROM plants *;";
+      String deleteJoinQuery = "DELETE FROM gardens_plants *;";
+      con.createQuery(deleteUserQuery).executeUpdate();
+      con.createQuery(deleteGardenQuery).executeUpdate();
+      con.createQuery(deletePlantQuery).executeUpdate();
+      con.createQuery(deleteJoinQuery).executeUpdate();
     }
   }
 
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+import java.io.UnsupportedEncodingException;
+import java.security.SecureRandom;
