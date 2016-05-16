561352653d45fe6b9bb57653f51b864abe660770
paul-hess <pauljacksonhess@gmail.com>
2016-05-16 11:54:19 -0700

    add basic route structures to app class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,11 +9,143 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
-    get("/", (req, response) -> {
+    // get
+    get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/home.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/sign-up", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/log-in", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/edit", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/garden-new", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/gardens", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/garden/:garden_id", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/garden/:garden_id/edit-garden", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/plants", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/plant-new", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/plants", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/plant/:plant_id", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    get("/user/:user_id/plant/:plant_id/edit-plant", (request, response) -> {
+    	Map<String, Object> model = new HashMap<String, Object>();
+    	return new ModelAndView(model, layout)
+    }, new VelocityTemplateEngine());
+
+    post("/sign-up", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/log-in", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/log-out", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/edit", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/garden-new", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/plant-new", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/garden/:garden_id/edit-garden", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/plant/:plant_id/edit-plant", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/search-plants", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/garden/:garden_id/plant/:plant_id/remove-garden-plant", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/garden/:garden_id/delete", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/delete-account", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
+    post("/user/:user_id/add-plant", (request, response) -> {
+    	response.redirect("");
+    	return null;
+    });
+
 
   }
 
